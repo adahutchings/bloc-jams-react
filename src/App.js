8e7b48932adef7397311a460034c7022f60e8049
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-03-22 16:33:06 -0700

    Assignment-3-Routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -10,14 +11,15 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/Library'>Library</Link>
+            <Link to='/'>Landing </Link>
+            <Link to='/Library'> Library</Link>
           </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/Library" component={Library} />
+          <Route path="/Album" component={Album} />
         </main>
       </div>
     );
---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+  <section className= "album">
+    These are albums!
+  </section>
+);
+
+export default Album;
