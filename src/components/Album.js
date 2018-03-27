9f63387922f001dc964d38b033c3db1dbb45b246
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-03-26 17:50:57 -0700

    Assignment 6 Components Album Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,6 +14,8 @@ class Album extends Component {
   }
 
   render() {
+
+
     return (
       <section className='album'>
         <section id="album-info">
@@ -21,7 +23,11 @@ class Album extends Component {
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}</div>
+            <div id="release-info">
+              {this.state.album.releaseInfo}
+              {this.state.album.year}
+              {this.state.album.label}
+            </div>
           </div>
         </section>
         <table id="song-list">
@@ -31,6 +37,17 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+          {
+            this.state.album.songs.map( (song, index) =>
+            <tr className="song" key={index}>
+              <td className="song-number">{index +1}</td>
+              <td className="song-title">{song.title}</td>
+              <td className="song-duration">{song.duration}</td>
+              <td className="ion-ios-play"></td>
+              <td className="ion-ios-pause"></td>
+            </tr>
+            )
+          }
           </tbody>
         </table>
       </section>
